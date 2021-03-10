ebaadf8b8257340fc19712696c4620a5f4b5f305
Nathan Peek <nmp14fsu@gmail.com>
2021-03-09 22:57:31 -0500

    Add database creation and table schema

---
+++ db/database.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS company_employee_db;
+
+CREATE DATABASE company_employee_db;
--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,26 @@
-DROP DATABASE IF EXISTS company_employee_db;
+USE company_employee_db;
 
-CREATE DATABASE company_employee_db;
+CREATE TABLE department (
+	id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+	id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+	id INT AUTO_INCREMENT,
+	first_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES role(id)
+);
