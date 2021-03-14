86c8c88906ce9073bd71776a3a2fd10637f5fc37
Nathan Peek <nmp14fsu@gmail.com>
2021-03-14 04:35:17 -0400

    Add prompt for user options and a switch for getting the function based on input

---
+++ main.js
@@ -0,0 +1,59 @@
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+const helperFunctions = require("./helper_function/queries");
+
+let connectionVar;
+
+const init = (connection) => {
+    connectionVar = connection;
+
+    // promptQuestions
+    mainFunction();
+}
+
+const mainFunction = async () => {
+    // Get user choice
+    const answer = await promptQuestions();
+    // Get function dependent on user choice
+    const selectedFunction = await getFunction(answer);
+
+    setTimeout(() => mainFunction(), 500);
+}
+
+const promptQuestions = () => {
+    return inquirer.prompt([
+        {
+            name: "options",
+            type: "rawlist",
+            choices: ["Get All Employees", "Add Department", "Add Roles", "Add Employee", "Update Employee Role", "View Departments", "View Roles"]
+        }
+    ]);
+}
+
+const getFunction = async (answer) => {
+    switch (answer.options) {
+        case "Get All Employees":
+            await helperFunctions.getAllEmployees(connectionVar);
+            break;
+        case "Add Department":
+            await helperFunctions.addDepartment(connectionVar);
+            break;
+        case "Add Roles":
+            await helperFunctions.addRoles(connectionVar);
+            break;
+        case "Add Employee":
+            await helperFunctions.addEmployee(connectionVar);
+            break;
+        case "Update Employee Role":
+            await helperFunctions.updateEmployee(connectionVar);
+            break;
+        case "View Departments":
+            await helperFunctions.viewDepartments(connectionVar);
+            break;
+        case "View Roles":
+            await helperFunctions.viewRoles(connectionVar);
+            break;
+    }
+}
+
+module.exports = init;
